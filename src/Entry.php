224fc3e1f12c5dc4e06784cfb0f723e1ac44a514
subbamaggus <markus.a.kerle@gmail.com>
2024-10-29 21:27:59 +0100

    adjust intending

--- src/Entry.php
+++ src/Entry.php
@@ -3,32 +3,32 @@ namespace PHPTestException;
 use InvalidArgumentException;
 final class Entry
 {
-   private $_id;
-   private $_value;
-   private $_description;
-   private $_timestamp;
-   
-   public function __construct(int $value, string $description, int $timestamp)
-   {
-       $this->id = -1;
-       $this->value = $value;
-       $this->description = $description;
-       $this->timestamp = $timestamp;
-   }
-   public function getId(): int
-   {
-       return $this->id;
-   }
-   public function getValue(): int
-   {
-       return $this->value;
-   }
-   public function getDescription(): string
-   {
-       return $this->description;
-   }
-   public function getTimestamp(): int
-   {
-       return $this->timestamp;
-   }
+    private $_id;
+    private $_value;
+    private $_description;
+    private $_timestamp;
+    
+    public function __construct(int $value, string $description, int $timestamp)
+    {
+        $this->id = -1;
+        $this->value = $value;
+        $this->description = $description;
+        $this->timestamp = $timestamp;
+    }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+    public function getTimestamp(): int
+    {
+        return $this->timestamp;
+    }
 }
--- tests/EntryTest.php
+++ tests/EntryTest.php
@@ -4,17 +4,25 @@ use PHPTestException\Entry;
 use PHPUnit\Framework\TestCase;
 final class EntryTest extends TestCase
 {
-   public function testCreateBogusIdAndReturnThis(): void
-   {
-       $myEntry = new Entry(12, "mydesc", 1234);
-       $this->assertEquals(-1, $myEntry->getId());
-       $this->assertEquals(12, $myEntry->getValue());
-       $this->assertEquals("mydesc", $myEntry->getDescription());
-       $this->assertEquals(1234, $myEntry->getTimestamp());
-   }
-   public function testNotInitiatedObjectFails(): void
-   {
-       $this->expectException(Error::class);
-       Entry::getValue();
+    public function testCreateBogusIdAndReturnThis(): void
+    {
+        $myEntry = new Entry(12, "mydesc", 1234);
+        $this->assertEquals(-1, $myEntry->getId());
+        $this->assertEquals(12, $myEntry->getValue());
+        $this->assertEquals("mydesc", $myEntry->getDescription());
+        $this->assertEquals(1234, $myEntry->getTimestamp());
+    }
+    public function testNotInitiatedObjectFails(): void
+    {
+        $this->expectException(Error::class);
+        Entry::getValue();
+    }
+    public function testCreateBogusIdAndReturnThis(): void
+    {
+        $myEntry = new Entry(12, "mydesc", 1234);
+        $this->assertEquals(-1, $myEntry->getId());
+        $this->assertEquals(12, $myEntry->getValue());
+        $this->assertEquals("mydesc", $myEntry->getDescription());
+        $this->assertEquals(1234, $myEntry->getTimestamp());
    }
 }
